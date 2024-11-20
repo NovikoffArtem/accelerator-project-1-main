6af05212ba2b6e14375cb01ba3b176596af0f0e6
NovikoffArtem <142929498+NovikoffArtem@users.noreply.github.com>
2024-11-20 03:32:30 +0300

    price/games

--- source/js/main.js
+++ source/js/main.js
@@ -2,3 +2,29 @@
 // import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
+
+
+const tabButtonsArr = Array.from(document.querySelectorAll('.price__tab-button'));
+
+let currentCount = document.querySelector('.price__tab-button--active');
+
+const changeTab = (evt) => {
+  if (evt.target.closest('button')){
+    evt.preventDefault();
+
+    const current = evt.target.closest('button');
+    currentCount.classList.remove('price__tab-button--active');
+    current.classList.add('price__tab-button--active');
+    currentCount = current;
+
+    const countOfMonths = Number(current.dataset.count);
+
+    const arr = Array.from(document.querySelectorAll('.price-card__count'));
+    arr.forEach((e) => {
+      const num = e.dataset.count * countOfMonths;
+      e.innerHTML = num;
+      e.dataset.price = num;
+    });
+  }
+};
+tabButtonsArr.forEach((e) => e.addEventListener('click', changeTab));
